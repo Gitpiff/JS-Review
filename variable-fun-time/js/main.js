bebc11aabffd19bf9f5ae0d2dd3e1627aafb7590
Victor <viknavarro@gmail.com>
2022-05-15 14:42:19 -0500

    updated variable solutions

--- variable-fun-time/js/main.js
+++ variable-fun-time/js/main.js
@@ -1,28 +1,36 @@
 //--- Easy
 //create a variable and assign it a number
-const num = 70
+let num = 70
 
 //minus 10 from that number
-const numMinus10 = num - 10
+// let numMinus10 = num - 10 // This solution is valid, just not ideal
+num -= 10 // This solution is much better
 
 //print that number to the console
-console.log(numMinus10)
+console.log(num)
 
 
 //--- Medium
 //create a variable that holds a value from the input
-const initInput = firstName.value
+//wrap the value in a Number() method to make sure that what we are getting is a number
+//let initInput = Number(document.querySelector("#danceDanceRevolution").value)
 
 //add 25 to that number
-const initInputPlus25 = initInput + 25
+//initInput += 25
 
 //alert that number
-alert(initInputPlus25)
+//alert(initInput)
 
 
 //--- Hard
 //create a variable that holds the h1
-const h1 = document.getElementsByTagName(h1)
+const h1 = document.querySelector("h1")
 
 //add an event listener to that element that console logs the sum of the two previous variables
-h1.addEventListener(onclick(console.log(numMinus10 + initInputPlus25)))
+h1.addEventListener("click", sumOfValues)
+
+function sumOfValues() {
+    let initInput = Number(document.querySelector("#danceDanceRevolution").value)
+    initInput += 25
+    console.log(num + initInput)
+}
