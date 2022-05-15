427bccdf4e52bb029ceabf8e8ba9efa7684db8db
Victor <viknavarro@gmail.com>
2022-05-15 16:30:34 -0500

    temp-converter

--- temperature-converter/js/main.js
+++ temperature-converter/js/main.js
@@ -1,1 +1,26 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+//Convert from C to F
+//Get the temp to convert in this case Celsius
+//Create a function that takes that temp as a parameter
+//Create a var called res, start it as an empty string
+//Code the formula to convert C to F, store the result into the var res
+//Return the result
+
+document.querySelector("h1").addEventListener("click", celciusToFarenheit)
+
+function celciusToFarenheit(c) {
+//     let f = c * 1.8 + 32
+
+//     console.log(f)
+//
+    let temp = Number(document.querySelector("input").value)
+
+    temp = temp * 1.8 + 32
+
+    document.querySelector("h2").innerText = temp
+
+
+}
+
+
+
