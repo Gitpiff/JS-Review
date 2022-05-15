35d8805ccd334da4690774ed4f1cab2c55458f26
Victor <viknavarro@gmail.com>
2022-05-15 14:19:16 -0500

    variables

--- variable-fun-time/js/main.js
+++ variable-fun-time/js/main.js
@@ -1,18 +1,28 @@
 //--- Easy
 //create a variable and assign it a number
+const num = 70
 
 //minus 10 from that number
+const numMinus10 = num - 10
 
 //print that number to the console
+console.log(numMinus10)
+
 
 //--- Medium
 //create a variable that holds a value from the input
+const initInput = firstName.value
 
 //add 25 to that number
+const initInputPlus25 = initInput + 25
 
 //alert that number
+alert(initInputPlus25)
+
 
 //--- Hard
 //create a variable that holds the h1
+const h1 = document.getElementsByTagName(h1)
 
 //add an event listener to that element that console logs the sum of the two previous variables
+h1.addEventListener(onclick(console.log(numMinus10 + initInputPlus25)))
